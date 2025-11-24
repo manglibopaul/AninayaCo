b7da0fe0af728651b7dd67c092f6d4b195b3c50c
Denzel Sadinoe <denzelsadino2@gmail.com>
2025-11-25 04:04:28 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {port : 5173},
-  base: "/aninaya-e-commerce"
+  plugins: [react()], 
+  base: process.env.VITE_BASE_PATH || "/aninaya-e-commerce",
 })
